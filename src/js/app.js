54d373a7ffb87f144f370909ac06ec77a34ddfca
Dabeldab <1dareios@gmail.com>
2025-04-03 01:20:39 +0000

    profile started

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,17 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>
+            ${variables.name === null ? `first` : variables.name}
+            ${variables.lastName === null ? `last` : variables.lastName}
+          </h1>
+          <h2>
+            ${variables.role === null ? `role` : variables.role}
+          </h2>
+          <h3>
+            ${variables.country === "null" ? `country` : variables.country},
+            ${variables.city === "null" ? `city` : variables.city}
+          </h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
